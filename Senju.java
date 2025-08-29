afb326b2ae52003d973aef52f4d63581ab53ca82
Vitor Gerhardt <163213295+vitugrt@users.noreply.github.com>
2025-08-29 18:22:25 -0300

    Add files via upload

--- Senju.java
+++ Senju.java
@@ -1,6 +1,8 @@
 package IntermediateLevel;
 
 public class Senju extends Hokage {
+
+    //abstract from Hokage
     @Override
     public void wisdom() {
         System.out.println("You're wisdom now");
