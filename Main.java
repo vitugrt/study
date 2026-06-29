f4e94cc5a49d773c96632938c27a9b43972149bf
Vitor Gerhardt <163213295+vitugrt@users.noreply.github.com>
2026-06-29 20:12:01 -0300

    set

--- Main.java
+++ Main.java
@@ -1,86 +1,34 @@
-package IntermediateLevel;
+package BasicLevel.DataType;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        List<String> ninjas = new ArrayList<>();
+        ninjas.add("Naruto");
+        ninjas.add("Naruto");
+        ninjas.add("Sasuke");
+        ninjas.add("Sakura");
+        ninjas.add("Kakashi");
+        ninjas.add("Akamaru");
+        ninjas.add("Akamaru");
+        ninjas.add("Zabuza");
+        //print the list normal in the implementation order, with index
+        System.out.println("The list of ninjas is: " +  ninjas);
 
-        //how to create a ninja
-        String name1 = "Naruto Uzumaki";
-        String village1 = "Konoha";
-        int age1 = 16;
+        Set<String> ninjasHash = new HashSet<>();
+        ninjasHash.addAll(ninjas);
+        //print the list without duplicates and without index
+        System.out.println("The HashSet of ninjas is: " + ninjasHash);
 
-        //another ninja
-        String name2 = "Sasuke Uchiha";
-        String village2 = "Konoha";
-        int age2 = 16;
+        Set<String> ninjasTree = new TreeSet<>();
+        ninjasTree.addAll(ninjas);
+        //print the list without duplicates, without index and in order
+        System.out.println("The TreeSet of ninjas is: " +  ninjasTree);
 
-        //how can i create a great amount of ninja? objects!
-
-        //object 1
-        Uzumaki naruto = new Uzumaki();
-        naruto.name = "Naruto Uzumaki";
-        naruto.village = "Konoha";
-        naruto.age = 17;
-        naruto.chakra();
-
-        //calling the string method
-        String shouting = naruto.hokage();
-        System.out.println(shouting);
-
-        //calling the int method
-        int howManyYears = naruto.ageToBeHokage( 70);
-        System.out.println("But I'm just " + naruto.age + " years and I have left "  + howManyYears + " years");
-
-        //object 2
-        Uchiha sasuke = new Uchiha();
-        sasuke.name = "Sasuke Uchiha";
-        sasuke.village = "Konoha";
-        sasuke.age = 18;
-        sasuke.sharinganON();
-
-        //calling the method, overwritten
-        sasuke.chakra();
-
-        //object 3
-        Ninja sakura = new Ninja();
-        sakura.name = "Sakura Haruno";
-        sakura.village = "Konoha";
-        sakura.age = 18;
-        sakura.chakra();
-
-        //object 4(using heredity)
-        Hyuga hinata = new Hyuga();
-        hinata.name = "Hinata Hyuga";
-        hinata.village = "Konoha";
-        hinata.age = 17;
-        String byakugan = hinata.byakuganON();
-        System.out.println("My name is " + hinata.name + " and my " + byakugan);
-        hinata.chakra();
-
-        //object 5(interface)
-        Boruto boruto = new Boruto();
-        boruto.name = "Boruto Uzumaki";
-        boruto.age = 9;
-        boruto.village = "Konoha";
-        boruto.activateKarma();
-        boruto.activateJougan();
-        boruto.byakuganON();
-        boruto.chakra();
-
-        //object 6(using constructors)
-        Senju tobirama = new Senju("Tobirama", 50, false);
-        System.out.println(tobirama.name);
-        System.out.println(tobirama.age);
-        System.out.println(tobirama.alive);
-
-        //object 7(class abstract) with no args
-        Senju hashirama = new Senju();
-        hashirama.wisdom();
-
-        //object 8(multiple hereditary)
-        Hatake kakashi = new Hatake();
-        kakashi.sharinganON();
-        kakashi.anbuNinja();
-        kakashi.hokageON();
-
+        Set<String> ninjasLinked = new LinkedHashSet<>();
+        ninjasLinked.addAll(ninjas);
+        //print the list without duplicates, without index and in order of implementation
+        System.out.println("The LinkedHashSet of ninjas is: " +  ninjasLinked);
     }
 }
